3d411a13c23accd4339b9cc2f5c2d2f345751274
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-21 14:17:31 -0700

    Add webpack and babel configs, compile chunk

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const outputDirectory = 'dist';
+
+module.exports = {
+  entry: {
+    index: "./src/portal/index.js",
+},
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "static/js/[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("development"),
+      },
+    }),
+  ],
+};
