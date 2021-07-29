e104fafeba558a76d3623de162d5387ef80cb762
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-07-28 23:02:29 -0700

    Add db reset code

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 
+
 const pool = new Pool ({
   user: 'portalserver',
   password: 'testpassword',
@@ -11,6 +12,46 @@ module.exports = {
 
   query: (text, params, callback) => {
     return pool.query(text, params, callback)
-  }
+  },
+
+
+  reset: () => {
+    
+    const runSchemaFiles = () => {
+      console.log(`-> Loading Schema Files ...`);
+      const schemaFiles = fs.readdirSync('./src/server/db/schema');
+
+      return Promise.all(schemaFiles.map(file => {
+        const sql = fs.readFileSync(`./src/server/db/schema/${file}`, 'utf8');
+        console.log(`---> Running ${file}`);
+        return pool.query(sql)
+      }))
+        .then(res => console.log('Schemas loaded'))
+        .catch(e => console.log(e))
+    };
+
+    const runSeedsFile = () => {
+      console.log(`-> Loading Seeds Files ...`);
+      const seedsFiles = fs.readdirSync('./src/server/db/seeds');
+
+      return Promise.all(seedsFiles.map(file => {
+        const sql = fs.readFileSync(`./src/server/db/seeds/${file}`, 'utf8');
+        console.log(`---> Running ${file}`);
+        return pool.query(sql)
+      }))
+        .then(res => console.log('Seeds loaded'))
+        .catch(e => console.log(e))
+    };
+
+    try {
+      // First run schema file, only after that seeds
+      runSchemaFiles()
+        .then(res => runSeedsFile())
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  xyz: () => console.log('123'),
   
 }
